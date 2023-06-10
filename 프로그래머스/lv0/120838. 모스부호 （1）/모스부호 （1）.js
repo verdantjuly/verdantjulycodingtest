9b8f707ed974c3de84bf4fa97a66e8ebfe210ed1
dayounglee <131671804+verdantjuly@users.noreply.github.com>
2023-06-10 12:31:41 +0900

    [level 0] Title: 모스부호 (1), Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,16 @@
+function solution(letter) {
+    let answer = []
+morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+
+    let letters = letter.split(" ")
+    letters.forEach(function (lettermorse){
+        answer.push(morse[lettermorse])
+    } )
+    return answer.join("")
+}
