c7abddb1a3a3a7517efe7a84e9f32a11dd1326d1
verdantjuly <131671804+verdantjuly@users.noreply.github.com>
2024-03-11 17:56:22 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i< signs.length ; i++){
+            if (!signs[i]){
+               answer = answer - absolutes[i];
+            } else{answer = answer + absolutes[i];}
+        }
+        
+        return answer;
+    }
+}
